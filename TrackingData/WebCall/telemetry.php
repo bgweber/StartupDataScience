96c4031059b676b95ee91d44fb338bd1f4ba9aac
Ben Weber <bgweber@gmail.com>
2018-04-30 19:44:53 -0700

    Create telemetry.php

---
+++ TrackingData/WebCall/telemetry.php
@@ -0,0 +1,9 @@
+ < ?php
+    $message = $_GET['message'];
+    if ($message != '') {
+        $dataFile = fopen("telemetry.log", "a");
+        fwrite($dataFile, "$message\n");
+        fflush($dataFile);
+        fclose($dataFile);
+    }
+?>

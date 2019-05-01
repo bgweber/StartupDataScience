7b2f75d474ced71be4ce71c8859ddac67e854ff4
Ben Weber <bgweber@gmail.com>
2019-04-30 19:13:48 -0700

    Create echo.py

---
+++ containers/echo/echo.py
@@ -0,0 +1,24 @@
+# load Flask 
+import flask
+app = flask.Flask(__name__)
+
+# define a predict function as an endpoint 
+@app.route("/predict", methods=["GET","POST"])
+def predict():
+    data = {"success": False}
+
+    # get the request parameters
+    params = flask.request.json
+    if (params == None):
+        params = flask.request.args
+
+    # if parameters are found, echo the msg parameter 
+    if (params != None):
+        data["response"] = params.get("msg")
+        data["success"] = True
+
+    # return a response in json format 
+    return flask.jsonify(data)
+
+# start the flask app, allow remote connections
+app.run(host='0.0.0.0')
